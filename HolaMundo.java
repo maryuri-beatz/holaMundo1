1132721225d05d2cd2e87ab51313e8599d2d4db4
maryuri-beatz <maryuribeatrizgarcia@gmail.com>
2022-08-29 13:46:05 -0600

    primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+/*Licencia de uso*/
+
+package com.mycompany.holamundo;
+
+/*Autor@colocha */
+
+public class HolaMundo {
+    public static void main (String[ ] args) {
+        System.out.println("Hola Mundo"); //operaciones
+      }
+}    
+// Una fincion,en java llamada clase esta compuesta por el nombre de la fincion 
+// y luego entre parentesis los tipos de datos que contendra la logica de programacion
+// dentro de la funcion
+        
